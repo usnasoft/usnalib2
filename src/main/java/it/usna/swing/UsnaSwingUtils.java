58db88eefe78d3793a54f276e5dec47bb9a864e8
aflaccomio <aflaccomio@gmail.com>
2023-04-12 20:06:01 +0200

    UsnaSwingUtils

---
+++ src/main/java/it/usna/swing/UsnaSwingUtils.java
@@ -0,0 +1,48 @@
+package it.usna.swing;
+
+import java.awt.Font;
+import java.util.Enumeration;
+
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.FontUIResource;
+
+public class UsnaSwingUtils {
+	public final static String LF_NMBUS = "Nimbus";
+
+	public static void setLookAndFeel(String lf) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if (lf.equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+	}
+	
+//	https://stackoverflow.com/questions/1043872/are-there-any-built-in-methods-in-java-to-increase-font-size
+	public static void initializeFontSize(float multiplier) {
+		UIDefaults defaults = UIManager.getDefaults();
+		for (Enumeration<Object> e = defaults.keys(); e.hasMoreElements();) {
+			Object key = e.nextElement();
+			Object value = defaults.get(key);
+//			System.out.println(key + "-" + value);
+			@SuppressWarnings("unused")
+			String dummy = value != null ? value.toString() : ""; // !!!
+			if (value instanceof Font) {
+				Font font = (Font) value;
+				float newSize = font.getSize() * multiplier;
+				if (value instanceof FontUIResource) {
+					defaults.put(key, new FontUIResource(font.deriveFont(newSize)));
+				} else {
+					defaults.put(key, font.deriveFont(newSize));
+				}
+			}
+		}
+	}
+}
