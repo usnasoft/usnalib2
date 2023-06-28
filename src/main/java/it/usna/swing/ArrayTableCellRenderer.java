08b73a248816d6e2a147d7d54aa751ff0f6394ed
aflaccomio <aflaccomio@gmail.com>
2023-06-29 00:15:19 +0200

    bug fix

--- src/main/java/it/usna/swing/ArrayTableCellRenderer.java
+++ src/main/java/it/usna/swing/ArrayTableCellRenderer.java
@@ -2,31 +2,34 @@ package it.usna.swing;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Insets;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
 import javax.swing.table.DefaultTableCellRenderer;
 
 public class ArrayTableCellRenderer extends DefaultTableCellRenderer {
 	private static final long serialVersionUID = 1L;
-//	private static Border EMPTY_BORDER;
-//	private final static Border FOCUS_BORDER = UIManager.getBorder("Table.focusCellHighlightBorder");
+	private static Border EMPTY_BORDER;
+	private final static Border FOCUS_BORDER = UIManager.getBorder("Table.focusCellHighlightBorder");
 	private JPanel p = new JPanel();
 
 	public ArrayTableCellRenderer() {
 		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-//		final Insets borderInsets = FOCUS_BORDER.getBorderInsets(this);
-//		EMPTY_BORDER = BorderFactory.createEmptyBorder(borderInsets.top, borderInsets.left, borderInsets.bottom, borderInsets.right);
+		final Insets borderInsets = FOCUS_BORDER.getBorderInsets(this);
+		EMPTY_BORDER = BorderFactory.createEmptyBorder(borderInsets.top, borderInsets.left, borderInsets.bottom, borderInsets.right);
 	}
 
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 		if(value instanceof Object[]) {
 			p.removeAll();
-//			final Color foregroundColor = isSelected ? table.getSelectionForeground() : table.getForeground();
-			final Color foregroundColor = this.getForeground();
+			final Color foregroundColor = isSelected ? table.getSelectionForeground() : table.getForeground();
 			for(Object v: (Object[])value) {
 				try {
 					JLabel l = new JLabel(v.toString());
@@ -34,7 +37,7 @@ public class ArrayTableCellRenderer extends DefaultTableCellRenderer {
 					p.add(l);
 				} catch(NullPointerException e) {}
 			}
-			p.setBorder(/*hasFocus ? FOCUS_BORDER : EMPTY_BORDER*/this.getBorder());
+			p.setBorder(hasFocus ? FOCUS_BORDER : EMPTY_BORDER);
 			return p;
 		}
 		return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
